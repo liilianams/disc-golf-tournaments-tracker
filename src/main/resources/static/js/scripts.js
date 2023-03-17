3583281a7c4e96bd4695f3d96ff645e8997891e9
liiliamalin <liilia.namsing@gmail.com>
2023-03-16 18:20:33 -0700

    Add Date and Tier filter

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,8 +1,34 @@
-function filterByLocation(location) {
+function resetFilters() {
+  document.getElementById("location-filter").selectedIndex = 0;
+  document.getElementById("month-filter").selectedIndex = 0;
+  document.getElementById("tier-filter").selectedIndex = 0;
+  filterTournaments();
+}
+
+function validateTier(rowTier, tier) {
+  if (rowTier?.includes('XC') && tier === 'C') {
+    return false;
+  }
+
+  return rowTier?.includes(tier);
+}
+
+function filterTournaments() {
+  const location = document.getElementById('location-filter').value;
+  const month = document.getElementById('month-filter').value;
+  const tier = document.getElementById('tier-filter').value;
   const rows = document.querySelectorAll('.tournament-row');
+
   rows.forEach(row => {
     const rowLocation = row.querySelector('.row-location').textContent;
-    if (!location || rowLocation === location) {
+    const rowMonth = row.querySelector('.row-date').textContent.split(' ')[0];
+    const rowTier = row.querySelector('.row-tier')?.textContent;
+
+    if (
+      (!location || rowLocation === location) &&
+      (!month || rowMonth.includes(month)) &&
+      (!tier || validateTier(rowTier, tier))
+    ) {
       row.style.display = '';
     } else {
       row.style.display = 'none';
